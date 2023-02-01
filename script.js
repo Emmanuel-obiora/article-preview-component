aaa4c2ebbd931b0e5a698d9785a80ebe4b878f32
Emmanuel-Obiora <emmanuelobiora94@gmail.com>
2023-02-01 13:56:02 +0100

    Adding desktop active state

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ let tog = true;
 function shareLinks(){
     if(tog){
         const share = document.getElementById('Share');
+        const trans = document.getElementById('transform');
 
         share.classList.add('show');
+        trans.classList.add('reveal');
         tog= false;
     } else{
         const quake = document.getElementById('Share');
+        const ret = document.getElementById('transform');
 
         quake.classList.remove('show');
+        ret.classList.remove('reveal');
         tog= true;
     }
     
